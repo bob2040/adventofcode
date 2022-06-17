0d9ef6972c199106a5c616dc41a34372797a2f19
bob <bob.xing@novacentrix.co.kr>
2022-06-17 16:09:55 +0800

    day3_binary_diagnostic_part2.py simplified

--- 2021_day3_binary_diagnostic/day3_binary_diagnostic_part2.py
+++ 2021_day3_binary_diagnostic/day3_binary_diagnostic_part2.py
@@ -5,38 +5,26 @@ https://adventofcode.com/2021/day/3#part2
 import day3_binary_diagnostic as d3bd
 
 
-def oxygen_rate(bin_l):
+def oxygen_rate(bin_l, s0, s1):
+    # 每行数据的位数 如：001001100101 12位
     ind = 12
     for i in range(ind):
         ret = [b[i] for b in bin_l for i in range(len(b))]
         if ret[i::ind].count('1') >= ret[i::ind].count('0'):
-            bin_l = [s for s in bin_l if s[i] == '1']
+            bin_l = [s for s in bin_l if s[i] == s1]
             # print(f'{i}过滤：', bin_l)
             if len(bin_l) == 1:
                 break
         else:
-            bin_l = [s for s in bin_l if s[i] == '0']
+            bin_l = [s for s in bin_l if s[i] == s0]
             # print(f'{i}过滤：', bin_l)
             if len(bin_l) == 1:
                 break
     return bin_l[0]
 
 
-def co2_rate(bin_l):
-    ind = 12
-    for i in range(ind):
-        ret = [b[i] for b in bin_l for i in range(len(b))]
-        if ret[i::ind].count('1') >= ret[i::ind].count('0'):
-            bin_l = [s for s in bin_l if s[i] == '0']
-            # print(f'{i}co2过滤：', bin_l)
-            if len(bin_l) == 1:
-                break
-        else:
-            bin_l = [s for s in bin_l if s[i] == '1']
-            # print(f'{i}co2过滤：', bin_l)
-            if len(bin_l) == 1:
-                break
-    return bin_l[0]
+def co2_rate(bin_l, s0, s1):
+    return oxygen_rate(bin_l, s0, s1)
 
 
 def life_support_rate(bin_o_r, bin_c_r):
@@ -44,11 +32,9 @@ def life_support_rate(bin_o_r, bin_c_r):
 
 
 if __name__ == '__main__':
-    # bin_l = d3bd.data_list('data_day3_binary_diagnostic')
     bin_l = d3bd.data_list('data_day3_binary_diagnostic')
     # print(bin_l)
-    # oc_l = oxygen_co2_rate(bin_l)
-    bin_o_r = oxygen_rate(bin_l)
-    bin_c_r =co2_rate(bin_l)
-    print(life_support_rate(bin_o_r, bin_c_r))
+    bin_o_r = oxygen_rate(bin_l, '0', '1')
+    bin_c_r =co2_rate(bin_l, '1', '0')
+    print(life_support_rate(bin_o_r, bin_c_r))  # 4412188
 
