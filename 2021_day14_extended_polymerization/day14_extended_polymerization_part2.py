5b7b05df0ca6d068f473fd1ea599b4402a083df0
bob <bob.xing@novacentrix.co.kr>
2022-09-19 09:11:36 +0800

    part2 improved

--- 2021_day14_extended_polymerization/day14_extended_polymerization_part2.py
+++ 2021_day14_extended_polymerization/day14_extended_polymerization_part2.py
@@ -1,37 +1,39 @@
 """
-Day 14: Extended Polymerization,Part Two
-https://adventofcode.com/2021/day/14#part2
+Day 14: Extended Polymerization
+https://adventofcode.com/2021/day/14
 """
-import numpy as np
 import time
-
+from itertools import chain
+from collections import Counter
 
 def get_diff(data_path):
     with open(data_path, 'r') as f:
         con = [s.strip() for s in f.readlines() if s != '\n']
     # print(con)
-    temp = np.array(list(con[0]))
+    temp = con[0]
     # print(temp)
     rule = dict([s.split(' -> ') for s in con if '->' in s])
     # print(rule)
     n = 0
     while n < 40:
+        new_t = []
+        for i in range(len(temp)-1):
+            te = tuple(temp[i:i + 2])
+            r = rule[te[0]+te[1]]
+            new_t.append(r)
+        # print(new_t)
+        last = temp[len(temp)-1]
+        temp = list(chain.from_iterable(zip(temp, new_t)))
+        temp.append(last)
         print(n)
-        for i in range(temp.size - 1):
-            temp = np.insert(temp, i*2+1, rule[temp[i*2]+temp[i*2+1]])
         n += 1
     # print(temp)
-    e = np.unique(temp)
-    t = []
-    for v in e:
-        t.append(np.sum(temp == v))
-    # print(quantity)
-    print(max(t) - min(t))
+    quantity = [temp.count(s) for s in set(temp)]
+    print(max(quantity) - min(quantity))
 
 
 if __name__ == '__main__':
     start = time.time()
     # get_diff('test')  #
-    get_diff('data_day14')
-    print(time.time() - start)
-
+    get_diff('data_day14')  #
+    print(time.time()-start)
